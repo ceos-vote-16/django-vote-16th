c7906c506d06902ccaad55323e8eea7d7b22c4e3
wjdtkdgns <wjdtkdgns329@naver.com>
2022-12-19 21:49:45 +0900

    🚀 feat : team, teammate 모델 생성 #13

--- api/models.py
+++ api/models.py
@@ -47,3 +47,19 @@ class User(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+
+class Team(AbstractUser, BaseModel):
+    name = models.CharField(max_length=150, unique=True)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class TeamVote(AbstractUser, BaseModel):
+    userPk = models.OneToOneField("User", on_delete=models.CASCADE)
+    teamPk = models.ForeignKey("Team", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "TeamVote"
